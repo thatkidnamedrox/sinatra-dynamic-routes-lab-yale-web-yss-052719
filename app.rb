3cd0b3456e4f37039fefb97e9b100b08efb2a94d
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-06-18 15:27:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,48 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name]
+    @name.reverse
+  end
+
+  get '/square/:number' do
+    @number = params[:number]
+    result = @number.to_i ** 2
+    "#{result}"
+  end
+
+  get '/say/:number/:phrase' do
+    @number = params[:number]
+    @phrase = params[:phrase]
+    result = ""
+    @number.to_i.times do |n|
+      result += @phrase
+    end
+    result
+  end
+
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
+    result = []
+    params.each do |key, value|
+      result << value
+    end
+    result.join(" ") + "."
+  end
+
+  get '/:operation/:number1/:number2' do
+    operations = {
+      add: lambda{|a,b| a + b },
+      subtract: lambda{|a,b| a - b },
+      multiply: lambda{|a,b| a * b },
+      divide: lambda{|a,b| a / b }
+    }
+    @op = params[:operation].to_sym
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    "#{operations[@op].call(@num1, @num2)}"
+  end
+
+
 
 end
